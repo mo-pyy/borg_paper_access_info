225ab6262d13974ffa56cc28ec423b8d441394d9
Moritz <moritz.krautwald@mailbox.org>
2023-01-26 11:34:13 +0100

    add ssh fingerprint randomart

--- paper_backup/main.py
+++ paper_backup/main.py
@@ -1,5 +1,6 @@
 from os import remove
 from subprocess import run
+from urllib.parse import urlparse
 from uuid import uuid4
 
 import click
@@ -8,8 +9,11 @@ import qrcode
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 
-def generate_pdf(html: str) -> str:
-    pdf_filepath = generate_tmp_file_path("borg_access_info.pdf")
+def generate_pdf(html: str, debug=False) -> str:
+    if debug:
+        pdf_filepath = "out.pdf"
+    else:
+        pdf_filepath = generate_tmp_file_path("borg_access_info.pdf")
     pdfkit.from_string(html, pdf_filepath, options={"enable-local-file-access": None})
     click.echo(f"Wrote pdf to file://{pdf_filepath}")
 
@@ -31,7 +35,7 @@ def create_ssh_qrcode(ssh_key_path: str) -> str:
     with open(ssh_key_path, "r") as f:
         ssh_private_key = f.read()
     img = qrcode.make(ssh_private_key)
-    img_path = generate_tmp_file_path("_ssh_qrcode.png")
+    img_path = generate_tmp_file_path("ssh_qrcode.png")
     img.save(img_path)
     return img_path
 
@@ -44,24 +48,61 @@ def create_ssh_key_melt(ssh_key_path: str) -> str:
         return result.stdout.decode()
 
 
+def get_hostname_from_url(url: str) -> str:
+    return urlparse(url).hostname
+
+
+def create_ssh_randomart(hostname: str, host_fingerprint: str):
+    result = run(
+        ["bash", "-c", f"ssh-keygen -lv -f <(ssh-keyscan -t ed25519 {hostname})"],
+        capture_output=True,
+    )
+    output = result.stdout.decode()
+    lines = output.splitlines()
+    reported_fingerprint = lines[0].split(" ")[1]
+    if not reported_fingerprint == host_fingerprint:
+        raise click.ClickException(
+            f"ssh fingerprints did not match. user specified {host_fingerprint} but server reported {reported_fingerprint}"  # noqa: long string
+        )
+    else:
+        return "\n".join(lines[1:])
+
+
 @click.command()
 @click.argument("ssh_key_path", type=click.Path(exists=True))
 @click.argument("borg_repo_url")
 @click.argument("borg_repo_ssh_fingerprint")
-def generate(ssh_key_path: str, borg_repo_url: str, borg_repo_ssh_fingerprint: str):
+@click.argument("borg_repo_password")
+@click.option("--debug", is_flag=True, default=False)
+def generate(
+    ssh_key_path: str,
+    borg_repo_url: str,
+    borg_repo_ssh_fingerprint: str,
+    borg_repo_password: str,
+    debug: bool,
+):
     """generate pdf with information to access you borg backup repo"""
+    randomart = create_ssh_randomart(
+        get_hostname_from_url(borg_repo_url), borg_repo_ssh_fingerprint
+    )
     ssh_key_melt = create_ssh_key_melt(ssh_key_path)
     ssh_qrcode_image_path = create_ssh_qrcode(ssh_key_path)
     vars = {
         "borg_repo_url": borg_repo_url,
         "borg_repo_ssh_fingerprint": borg_repo_ssh_fingerprint,
+        "borg_repo_password": borg_repo_password,
         "ssh_key_melt": ssh_key_melt,
         "ssh_qrcode_image_path": ssh_qrcode_image_path,
+        "borg_repo_ssh_fingerprint_randomart": randomart,
     }
     html = render_html(vars)
-    generate_pdf(html)
-    remove(ssh_qrcode_image_path)
+    if debug:
+        with open("out.html", "w") as f:
+            f.write(html)
+    generate_pdf(html, debug)
+    if not debug:
+        remove(ssh_qrcode_image_path)
 
 
 if __name__ == "__main__":
-    generate()
+    generate(auto_envvar_prefix="BORG_PAPER_ACCESS_INFO")
